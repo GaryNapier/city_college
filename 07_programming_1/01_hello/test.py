82c3d9320b3fe533b63e069c6393e10931ba305f
GaryNapier <garynapier83@googlemail.com>
2026-06-20 17:27:42 +0100

    add two more tests

--- 07_programming_1/01_hello/test.py
+++ 07_programming_1/01_hello/test.py
@@ -24,3 +24,19 @@ def test_executable():
     out = getoutput(prg)
     assert out.strip() == 'Hello, World!'
 
+def test_usage():
+    """usage"""
+
+    for flag in ['-h', '--help']:
+        rv, out = getstatusoutput(f'{prg} {flag}')
+        assert rv == 0
+        assert out.lower().startswith('usage')  
+
+def test_input():
+    """test for input"""
+
+    for val in ['Universe', 'Multiverse']:
+        for option in ['-n', '--name']:
+            rv, out = getstatusoutput(f'{prg} {option} {val}')
+            assert rv == 0
+            assert out.strip() == f'Hello, {val}!'
